d2bd3c10001293215124e5c11bf9190de4d3adc6
romijatmiko <romijatmiko033@gmail.com>
2022-12-26 20:55:17 +0700

    d

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,9 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
-	host: PGHOST,
-	dialect: "postgres",
-});
+const db = new Sequelize(
+	process.env.PGDATABASE,
+	process.env.PGUSER,
+	process.env.PGPASSWORD,
+	{
+		host: process.env.PGHOST,
+		port: process.env.PGPORT,
+		dialect: "postgres",
+	}
+);
 // const db = new Sequelize(
 // 	"postgresql://postgres:S44fCmIyba5WJ7yCuv6m@containers-us-west-66.railway.app:5727/railway"
 // );
