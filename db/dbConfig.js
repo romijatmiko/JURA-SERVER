aab2294cd148af761f2b27997917114fc226f613
romijatmiko <romijatmiko033@gmail.com>
2022-12-26 21:52:16 +0700

    new config

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,17 +1,17 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-	process.env.PGDATABASE,
-	process.env.PGUSER,
-	process.env.PGPASSWORD,
-	{
-		host: process.env.PGHOST,
-		port: process.env.PGPORT,
-		dialect: "postgres",
-	}
-);
 // const db = new Sequelize(
-// 	"postgresql://postgres:S44fCmIyba5WJ7yCuv6m@containers-us-west-66.railway.app:5727/railway"
+// 	process.env.PGDATABASE,
+// 	process.env.PGUSER,
+// 	process.env.PGPASSWORD,
+// 	{
+// 		host: process.env.PGHOST,
+// 		port: process.env.PGPORT,
+// 		dialect: "postgres",
+// 	}
 // );
+const db = new Sequelize(
+	"postgres://jura-backend-main-db-02ff78056a242c4f4:abfzvpcQfgCGwKHEf2UptNEUqhhgJZ@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/jura-backend-main-db-02ff78056a242c4f4"
+);
 
 export default db;
