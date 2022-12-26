ea817c4636685400d88f4c3dd9856307b62f5183
romijatmiko <romijatmiko033@gmail.com>
2022-12-26 20:48:39 +0700

    za

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,7 +1,11 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-	"postgresql://postgres:S44fCmIyba5WJ7yCuv6m@containers-us-west-66.railway.app:5727/railway"
-);
+const db = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+	host: PGHOST,
+	dialect: "postgres",
+});
+// const db = new Sequelize(
+// 	"postgresql://postgres:S44fCmIyba5WJ7yCuv6m@containers-us-west-66.railway.app:5727/railway"
+// );
 
 export default db;
