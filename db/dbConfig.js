795a1042d5e928000e1dd197025afbc0f46c0b41
romijatmiko <romijatmiko033@gmail.com>
2022-12-26 23:23:17 +0700

    yea

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -11,7 +11,7 @@ import { Sequelize } from "sequelize";
 // 	}
 // );
 const db = new Sequelize(
-	"postgresql://postgres:S44fCmIyba5WJ7yCuv6m@containers-us-west-66.railway.app:5727/railway"
+	"postgres://jura-backend-main-db-02ff78056a242c4f4:abfzvpcQfgCGwKHEf2UptNEUqhhgJZ@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/jura-backend-main-db-02ff78056a242c4f4"
 );
 
 export default db;
--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(
 		saveUninitialized: true,
 		store: store,
 		cookie: {
+			httpOnly: false,
 			secure: "auto",
 		},
 	})
