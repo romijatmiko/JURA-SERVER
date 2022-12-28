aad94db847e6e4c3794a6f036fea15d09c41f1eb
romijatmiko <romijatmiko033@gmail.com>
2022-12-28 23:48:55 +0700

    try

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,13 @@ import { Login, logOut, Me } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/me", Me);
+router.get("/me", (_req, res) => {
+	res.setHeader(
+		"Set-Cookie",
+		"name=www.jurachicken.my.id; domain=https://www.jurachicken.my.id"
+	);
+	res.send("Cookie set"), Me;
+});
 router.post("/login", Login);
 router.delete("/logout", logOut);
 
